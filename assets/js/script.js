62d2464d66bb68ee4278d5bf9932c72e6dbb44d5
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-04-14 19:49:27 -0700

    Set our getElementByIds and checkboxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,32 @@
 console.log("hello from script.js");
 
-function prompt()
+// prompt()
 
-function prompt()
+// prompt() 
+
+document.getElementById("Password-Length");
+    
+document.getElementById("Generator").addEventListener("click", function (event){
+    var checkboxes = {
+        lowercase:(document.getElementById("lowercase").checked),
+        uppercase:(document.getElementById("uppercase").checked),
+        numeric:(document.getElementById("numeric").checked),
+        specialCharacters:(document.getElementById("special-characters").checked),
+        passwordLength:parseInt(document.getElementById("Password-Length").value),
+    }
+    // console.log(checkboxes)
+    // console.log(checkboxes.numeric)
+
+    if (checkboxes.passwordLength < 8 || checkboxes.passwordLength > 128) {
+        alert("Password needs to be between 8 and 128 characters");
+    }
+    else {
+        // This is where the logic will go for generating a password
+
+        // Will need a repository of uppercase characters in arrays,
+
+        // set that array and then generate a random number that will now take that index into the array
+
+        
+    }
+})
