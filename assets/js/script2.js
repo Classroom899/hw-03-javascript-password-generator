348d30e1810d77ba471b4d691ddbf35139e1f298
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-04-18 15:49:28 -0700

    Add javascript changes, add new generate password function, and if statements

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,29 +1,64 @@
 var generateBtn = document.querySelector("#Generator");
 
-var lowercase = (document.getElementById("lowercase").checked);
-var uppercase = (document.getElementById("uppercase").checked);
-var numeric = (document.getElementById("numeric").checked);
-var specialCharacters = (document.getElementById("special-characters").checked);
-var passwordLength = document.getElementById("Password-Length").value;
+var lowercase = document.getElementById("lowercase");
+var uppercase = document.getElementById("uppercase");
+var numeric = document.getElementById("numeric");
+var specialCharacters = document.getElementById("special-characters");
+var passwordEl = document.getElementById("Password-Length");
 
 // Set the characters below 
 
-//Function called generatePassword
-
-//Then inside generatePassword it needs to check what checkboxes are checked - so using if statements and into the string
-
-
-// Reference for loop, set a variable to use instead of numbers in my math.randoms
-
-
+var lowercaseOptions = "abcdefghijklmnopqrstuvwxyz"
+//var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+//var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
+//var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 
 function writePassword() {
+    var passwordLength = parseInt(passwordEl.value);
     if (passwordLength < 8 || passwordLength > 128) {
         alert("Password needs to be between 8 and 128 characters in length");
     } else {
-        var password = generatePassword();
-        var passwordText = document.querySelector("password");
-        passwordText.value = passwords;
+        var password = generatePassword(passwordLength);
+        var passwordText = document.querySelector("#password");
+        passwordText.value = password;
     }
 }
 generateBtn.addEventListener("click", writePassword);
+// var charSet = {
+//     lowercaseLength = "lowercase.length",
+//     uppercaseLength = "uppercase.length",
+//     numericLength = "numeric.length",
+//     specialCharactersLength = "specialCharacters.length",
+// };
+
+
+//Function called generatePassword
+
+function generatePassword(length) {
+    var generatedPassword = "";
+    // Get criteria from page
+    var criteria = {
+        lowercase: lowercase.checked
+    }
+    console.log(criteria);
+    // Generate a list of allowed characters based on criteria
+    var allowedOptions = [];
+    if(criteria.lowercase) {
+        allowedOptions = allowedOptions + lowercaseOptions;
+    }
+    allowedOptions = allowedOptions.split("") 
+    // Write a for loop from 0 to less than length
+    for (var i = 0; i < length; i++) {
+        var character = allowedOptions[Math.floor(Math.random() * (allowedOptions.length - 1))]
+        generatedPassword = generatedPassword + character
+    };
+        // Pick a random character from allowed options
+        // Concatentate that in to generaatedPassword
+    // Return final password
+    return generatedPassword;
+}
+
+//Then inside generatePassword it needs to check what checkboxes are checked - so using if statements and into the string
+
+    // Reference for loop, set a variable to use instead of numbers in my math.randoms
+    // for (var 1 = 0; i < )
